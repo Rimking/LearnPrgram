e2b946bf179bbc4868270ec0b94b472822f13ac6
Rimking <352293074@qq.com>
2020-06-25 16:10:55 +0800

    初体验,数据绑定、循环、事件

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,33 @@
 // pages/home/home.js
 Page({
+  data:{
+    name:'aY',
+    counter:0,
+    students:[
+      {id:11,name:'kebe',age:30},
+      {id:13,name:'zms',age:35},
+      {id:12,name:'md',age:21},
+    ]
+  },
+  handleBtnclick(){
+    // 错误的做法：界面不会发生变化
+      //   this.data.counter += 1
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+      // this.setDate()
+      this.setData({
+        counter:this.data.counter + 1
+      })
+
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  handleBtnJ(){
+     // this.setDate()
+     this.setData({
+      counter:this.data.counter - 1
+    })
 
   }
+
+
+
 })
